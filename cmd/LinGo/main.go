1771a857197a19676a7236f0104a112f27b10246
MonkieeBoi <danny.pham26425@gmail.com>
2025-04-11 13:05:36 +1000

    feat: tile widget

--- cmd/LinGo/main.go
+++ cmd/LinGo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"os"
@@ -9,6 +10,7 @@ import (
 	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/paint"
+	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
@@ -28,15 +30,47 @@ func main() {
 	app.Main()
 }
 
+func tile(th *material.Theme, s string) layout.FlexChild {
+	return layout.Rigid(
+		func(gtx C) D {
+			margins := layout.Inset{
+				Top:    unit.Dp(25),
+				Bottom: unit.Dp(25),
+				Right:  unit.Dp(5),
+				Left:   unit.Dp(5),
+			}
+			border := widget.Border{
+				Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 0xFF},
+				CornerRadius: unit.Dp(3),
+				Width:        unit.Dp(2),
+			}
+			return margins.Layout(gtx,
+				func(gtx C) D {
+					return border.Layout(gtx,
+						func(gtx C) D {
+							textH4 := material.H4(th, s)
+							textH4.Alignment = text.Middle
+							gtx.Constraints.Min.X = int(unit.Dp(33))
+							return layout.UniformInset(unit.Dp(10)).Layout(gtx, textH4.Layout)
+						},
+					)
+				},
+			)
+		},
+	)
+}
+
 func run(window *app.Window) error {
 	window.Option(app.Title("LinGo"))
-	theme := material.NewTheme()
-	theme.Palette.Bg = color.NRGBA{46, 52, 64, 0xFF}
-	theme.Palette.Fg = color.NRGBA{0xFF, 0xFF, 0xFF, 0xFF}
+	th := material.NewTheme()
+	th.Palette.Bg = color.NRGBA{46, 52, 64, 0xFF}
+	th.Palette.Fg = color.NRGBA{0xFF, 0xFF, 0xFF, 0xFF}
+	th.Palette.ContrastBg, th.Palette.ContrastFg = th.Palette.ContrastFg, th.Palette.ContrastBg
 	textInput := widget.Editor{
-		SingleLine: true,
 		Filter:     "abcdefghijklmnopqrstuvwxyz",
 		MaxLen:     15,
+		SingleLine: true,
+		Submit:     true,
 	}
 	var ops op.Ops
 	for {
@@ -46,17 +80,24 @@ func run(window *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 			paint.Fill(&ops, color.NRGBA{46, 52, 64, 0xFF})
+			for tev, ok := textInput.Update(gtx); ok; tev, ok = textInput.Update(gtx) {
+				switch tev.(type) {
+				case widget.SubmitEvent:
+					word := tev.(widget.SubmitEvent).Text
+					textInput.SetText("")
+					fmt.Println(word)
+				}
+			}
 			layout.Flex{
-				Axis:    layout.Vertical,
-				Spacing: layout.SpaceEnd,
+				Axis:      layout.Vertical,
+				Spacing:   layout.SpaceEnd,
+				Alignment: layout.Middle,
 			}.Layout(gtx,
 				layout.Rigid(
 					func(gtx C) D {
 						margins := layout.Inset{
 							Top:    unit.Dp(25),
 							Bottom: unit.Dp(25),
-							Right:  unit.Dp(80),
-							Left:   unit.Dp(80),
 						}
 						border := widget.Border{
 							Color:        color.NRGBA{R: 204, G: 204, B: 204, A: 0xFF},
@@ -67,11 +108,28 @@ func run(window *app.Window) error {
 							func(gtx C) D {
 								return border.Layout(gtx,
 									func(gtx C) D {
+										gtx.Constraints.Max.X = int(unit.Dp(400))
 										return layout.UniformInset(unit.Dp(10)).Layout(gtx,
-											material.Editor(theme, &textInput, "Enter Word").Layout)
+											material.Editor(th, &textInput, "Enter Word").Layout)
 									},
 								)
 							},
+						)
+					},
+				),
+				layout.Rigid(
+					func(gtx C) D {
+						return layout.Flex{
+							Axis:    layout.Horizontal,
+							Spacing: layout.SpaceSides,
+						}.Layout(gtx,
+							tile(th, "A"),
+							tile(th, "E"),
+							tile(th, "I"),
+							tile(th, "N"),
+							tile(th, "R"),
+							tile(th, "S"),
+							tile(th, "T"),
 						)
 					},
 				),
