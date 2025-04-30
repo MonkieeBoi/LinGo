25644fcb4cd2b7aba4172a8e7e9df8367f39cc89
MonkieeBoi <danny.pham26425@gmail.com>
2025-05-01 00:20:26 +1000

    fix: sort found words

--- internal/components/found.go
+++ internal/components/found.go
@@ -1,6 +1,8 @@
 package components
 
 import (
+	"sort"
+
 	"gioui.org/layout"
 	"gioui.org/text"
 	"gioui.org/widget/material"
@@ -12,21 +14,27 @@ func layoutWord(th *material.Theme, word string) layout.FlexChild {
 	return layout.Rigid(h4.Layout)
 }
 
-func layoutWords(th *material.Theme, found map[string]bool) []layout.FlexChild {
-	words := make([]layout.FlexChild, 0)
-	for word := range found {
+func layoutWords(th *material.Theme, found []string) []layout.FlexChild {
+	words := []layout.FlexChild{}
+	for _, word := range found {
 		words = append(words, layoutWord(th, word))
 	}
 	return words
 }
 
 func LayoutFound(d *data, u *ui) func(C) D {
+	words := []string{}
+	for word := range d.found {
+		words = append(words, word)
+	}
+	sort.Strings(words)
+
 	return func(gtx C) D {
 		return layout.Flex{
 			Axis:    layout.Vertical,
 			Spacing: layout.SpaceEnd,
 		}.Layout(gtx,
-			layoutWords(u.th, d.found)...,
+			layoutWords(u.th, words)...,
 		)
 	}
 }
