0a99a3e783930e89345d1189f14d6213985e804c
MonkieeBoi <danny.pham26425@gmail.com>
2025-05-03 13:48:29 +1000

    feat: proper contrast colours

--- internal/theme/theme.go
+++ internal/theme/theme.go
@@ -7,6 +7,8 @@ import (
 )
 
 var nord0 = color.NRGBA{46, 52, 64, 0xFF}
+var nord1 = color.NRGBA{59, 66, 82, 0xFF}
+var nord4 = color.NRGBA{216, 222, 233, 0xFF}
 var white = color.NRGBA{0xFF, 0xFF, 0xFF, 0xFF}
 
 func NewTheme() *material.Theme {
@@ -14,7 +16,8 @@ func NewTheme() *material.Theme {
 
 	th.Palette.Bg = nord0
 	th.Palette.Fg = white
-	th.Palette.ContrastBg, th.Palette.ContrastFg = th.Palette.ContrastFg, th.Palette.ContrastBg
+	th.Palette.ContrastBg = nord1
+	th.Palette.ContrastFg = nord4
 
 	return th
 }
